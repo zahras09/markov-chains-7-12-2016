e2e10abcf721fed358133d36e8f72d2cd36847e1
Hackbright Student <info@hackbrightacademy.com>
2016-07-13 12:05:16 -0700

    added comments

--- markov.py
+++ markov.py
@@ -46,21 +46,46 @@ def make_chains(text_string):
         # re-assign the key and use .get function to go over every key, if it does exist,
         # return the key if it doesn't exist return an empty list.
         chains[key] = chains.get(key, [])
-        # if key in chains appendto its value.
         chains[key].append(words[i+2])
     return chains
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
+    #creates empty list
+    words = []
+    #picks random key and stores in variable
+    current_key = choice(chains.keys())
+    #unpacks tuple and moves each word of the key into words list
+    for item in current_key:
+        words.append(item)
+    #concatenates strings into one string and stores in variable
+    text = words[0] + " " + words[1]
+    #using current key, we call list of values. Choice randomly chooses 
+    #one value and stores in value as string
+    chosen_word = choice(chains[current_key])
+    #concatenate chosen word to text (string)
+    text += " " + chosen_word
 
-    text = ""
-
-    # your code goes here
+    while True: 
+        #creates new key(tuple) with the second word in current_key
+        #and chosen word
+        new_key = (current_key[1], chosen_word)
+        #if the new_key is the last two words of all the words in the
+        #file, break from the loop
+        if chains.get(new_key) == None:
+            break
+        #use new key to call values and randomly choose one and store 
+        #the randomly chosen value in chosen_word    
+        chosen_word = choice(chains[new_key])
+        #updates current_key with the contents of new_content
+        current_key = new_key
+        #adds the chosen_word to the string text
+        text += " " + chosen_word
 
     return text
 
 
-input_path = "green-eggs.txt"
+input_path = "gettysburg.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
